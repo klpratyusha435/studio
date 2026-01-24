4c6a9a7359de3e62653ed28f55e997a0dd186e32
Lakshmi Pratyusha Kasturi <g25026@astra.xlri.ac.in>
2026-01-24 06:08:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4875962305-951b4",
+  "appId": "1:826782664288:web:9281503263216a83656505",
+  "apiKey": "AIzaSyCCupg59h_aQZq-oMPSfWqQrrGfNahFhcY",
+  "authDomain": "studio-4875962305-951b4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "826782664288"
+};
